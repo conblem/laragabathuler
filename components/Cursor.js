5f5d3592ca0029caaf7aa92849c31ceb73aaa1cc
conblem <mail@conblem.me>
2021-06-04 15:00:06 +0200

    remove unused import

--- components/Cursor.js
+++ components/Cursor.js
@@ -1,5 +1,5 @@
 import styles from "../styles/Cursor.module.scss";
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect } from "react";
 
 export default function Cursor() {
   const animationFrame = useRef();
