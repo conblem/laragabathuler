896acf4a23f7a7aaae9cd2c01fcb7d80bf8161cc
conblem <mail@conblem.me>
2021-06-07 09:24:19 +0200

    experiment

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,62 @@
+import dynamic from "next/dynamic";
+import React from "react";
+import { LazyMotion } from "framer-motion";
+
 import "../styles/globals.scss";
 import Layout from "../components/Layout";
 
+const loadFeatures = () => import("../lib/features").then((res) => res.default);
+
+const ChildContext = React.createContext("AnimateSharedLayout");
+
+const delay = (amount) =>
+  new Promise((res) => {
+    console.log("start delay");
+    setTimeout(res, amount);
+  });
+
+const AnimateSharedLayout = dynamic(
+  () =>
+    delay(2000)
+      .then(() => import("../lib/animateSharedLayout"))
+      .then((res) => res.default)
+      .then((AnimateSharedLayout) => () => {
+        console.log("loaded animate shared layout");
+        return (
+          <ChildContext.Consumer>
+            {(children) => (
+              <AnimateSharedLayout>{children}</AnimateSharedLayout>
+            )}
+          </ChildContext.Consumer>
+        );
+      }),
+  {
+    loading: () => (
+      <ChildContext.Consumer>{(children) => children}</ChildContext.Consumer>
+    ),
+    ssr: false,
+  }
+);
+
+const LazyAnimateSharedLayout = ({ children }) => {
+  return process.browser ? (
+    <ChildContext.Provider value={children}>
+      <AnimateSharedLayout />
+    </ChildContext.Provider>
+  ) : (
+    children
+  );
+};
+
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <LazyMotion features={loadFeatures}>
+      <LazyAnimateSharedLayout>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </LazyAnimateSharedLayout>
+    </LazyMotion>
   );
 }
 
