c59b0674180ec00deec02456e15a82370b38fad8
conblem <mail@conblem.me>
2021-06-13 20:57:58 +0200

    wip

--- components/Content.js
+++ components/Content.js
@@ -1,18 +1,27 @@
 import styles from "../styles/Content.module.scss";
+import Maske from "../public/maske.svg";
 
 export default function Content({ hover, children, aspectX, aspectY }) {
   const paddingTop = `calc(${aspectY} / ${aspectX} * 100%)`;
   return (
     <div style={{ paddingTop }} className={styles.content}>
       <div className={`${styles.cover} ${styles.center}`}>
-        <div className={styles.ripple}>
+        <div className={styles.ring}>
+          <div></div>
+          <div></div>
           <div></div>
           <div></div>
         </div>
       </div>
       <div className={styles.cover}>{children}</div>
       {hover && (
-        <div className={`${styles.hover} ${styles.cover}`}>{hover}</div>
+        <div className={`${styles.hover}`}>
+          <Maske className={styles.top} />
+          <div className={styles.textWrapper}>
+            <div className={styles.text}>{hover}</div>
+          </div>
+          <Maske className={styles.right} />
+        </div>
       )}
     </div>
   );
