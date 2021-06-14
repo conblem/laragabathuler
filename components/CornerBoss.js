c0d8374d0cb1fad6858765815187edc7a2e6c0ea
conblem <mail@conblem.me>
2021-06-14 10:17:14 +0200

    some additions and remove lottie

--- components/CornerBoss.js
+++ components/CornerBoss.js
@@ -11,7 +11,6 @@ const calculate = (setElem, width) =>
         return;
       }
       const { left, right } = node.getBoundingClientRect();
-      console.log({ left, right });
       setElem({ left, right });
     },
     [width]
@@ -56,7 +55,7 @@ function Corners({ left, right }) {
       <Maske
         className={styles.corner}
         style={{
-          transform: `translateX(${right - 15}px) rotate(180deg)`,
+          transform: `translateX(${right - 14}px) rotate(180deg)`,
         }}
       />
     </>
--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,25 @@
 import Link from "next/link";
 import Head from "next/head";
 import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
 
-import NorthEast from "../public/northEast.svg";
+import Arrow from "../public/arrow.svg";
 import styles from "../styles/Layout.module.scss";
 
 const Cursor = dynamic(() => import("./Cursor"), {
   ssr: false,
 });
+
+function ActiveLink({ href, children, ...props }) {
+  const { asPath } = useRouter();
+
+  const className = asPath == href ? styles.active : undefined;
+  return (
+    <Link href={href} {...props}>
+      {children(className)}
+    </Link>
+  );
+}
 
 export default function Layout({ children }) {
   return (
@@ -28,15 +40,15 @@ export default function Layout({ children }) {
               Lara <span className={styles.lastName}>Gabathuler</span>
             </a>
           </Link>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
+          <ActiveLink href="/about">
+            {(active) => <a className={active}>About</a>}
+          </ActiveLink>
         </nav>
       </header>
       <div className={styles.children}>{children}</div>
       <footer className={styles.footer}>
         <a className={styles.mail} href="mailto:laragaba@hotmail.ch">
-          Mail <NorthEast />
+          Mail <Arrow />
         </a>
         <p className={styles.copyright}>
           ©2021 Lara Gabathuler. All rights reserved.
